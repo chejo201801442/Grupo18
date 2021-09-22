1dad665c60356c899c74bf8049d8c99f79632c51
Sergio <sergioling123@gmail.com>
2021-09-21 23:14:54 -0600

    a

--- scriptcarga.sh
+++ scriptcarga.sh
@@ -2,6 +2,5 @@ sqlldr sho/1234 control=carga.ctl errors=5000
 sqlldr sho/1234 control=carga2.ctl errors=5000
 sqlldr sho/1234 control=carga3.ctl errors=5000
 sqlldr sho/1234 control=carga4.ctl errors=5000
-sqlldr sho/1234 control=carga5.ctl errors=5000
-sqlplus @update.sql
+sqlldr sho/1234 control=carga5.ctl errors=5000 
 exit
